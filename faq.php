9496e3dccb615f622948638be828b0b4dca88b10
Even70000 <75841107+Even70000@users.noreply.github.com>
2021-01-29 11:43:31 +0100

    navbar bij elkaar gelinkt

---
+++ faq.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/6488e6347e.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/navbar.css" />
+</head>
+<body>
+       <!-- <img src="images/mk5.png"> -->
+       <img class="logovista" src="images/logovista.webp" alt="">
+  <!-- #region Navbar-->
+  <div id="navbar"><script>
+    
+    $("#navbar").load("common/navbar.html");
+  </script></div>
+ 
+  <!-- #endregion -->
+</body>
+</html>
